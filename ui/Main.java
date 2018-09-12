d6fe864e849590f95850a33b8a9db76f45cbe35f
Nicholas Kao <kobenkao@gmail.com>
2018-09-11 20:53:03 -0700

    Week 1 "Get Started!" commit
    -created ui package
    -created main class
    -created two methods that print out statements after running main

---
+++ ui/Main.java
@@ -0,0 +1,19 @@
+package ui;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        projectMessage();
+        javaMessage();
+    }
+
+    public static void projectMessage() {
+        System.out.println(" Team189 Project ");
+    }
+
+    public static void javaMessage() {
+        System.out.println(" Java is pretty pretty");
+    }
+
+}
