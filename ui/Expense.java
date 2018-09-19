07cd176461f25d1aece8cea556c1db4e986da0ba
Nicholas Kao <kobenkao@gmail.com>
2018-09-19 01:04:43 -0700

    P2 commit
    added two new calsses expense and income and changed main class to
    Budget. New text menu asking for options that is interacted through the
    text editor

---
+++ ui/Expense.java
@@ -0,0 +1,17 @@
+package ui;
+
+public class Expense {
+
+    private int cost;
+    private String name;
+    private String category;
+
+    public Expense(int cost, String name, String category) {
+        this.cost = cost;
+        this.name = name;
+        this.category = category;
+    }
+
+
+
+}
---
+++ ui/Income.java
@@ -0,0 +1,11 @@
+package ui;
+
+public class Income {
+
+    private int amount;
+
+    public Income (int amount) {
+        this.amount = amount;
+    }
+
+}
