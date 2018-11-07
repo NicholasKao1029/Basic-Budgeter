3cc40da74dbdea648e70daef2b3712e361fa7345
Nicholas Kao <kobenkao@gmail.com>
2018-11-07 09:18:22 -0800

    refactoring to have a new choice management class,if you want to revert just delete choiceManagement as of now

--- src/model/Budgeting/Salaries.java
+++ src/model/Budgeting/Salaries.java
@@ -1,10 +1,13 @@
 package model.Budgeting;
 
 import model.Income.Salary;
+import model.FileParser.IncomeJSON;
 
 import java.util.List;
 
 public class Salaries {
 
     private List<Salary> salaries;
+    private IncomeJSON incomeParser = new IncomeJSON();
+
 }
